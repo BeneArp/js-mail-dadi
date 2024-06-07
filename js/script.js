c57709af9b2bc0018a47b7bebee2e5f71d52eab6
BeneArp <benedetta.arpino@gmail.com>
2024-06-07 17:15:39 +0200

    creazione arrey

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
+
+// array
+const indirizziEmail = ["ginopippo@gmail.com", "giacomello@alice.it", "artemisia@gmail.com", "benedetta.arpino@gmail.com"]
+
+
+// creo una variabile che contiene quello che digita l'utente
+const emailUtene = document.querySelector("input");
+
+
