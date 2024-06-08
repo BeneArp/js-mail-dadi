cc22868f12345ee290f1f4c98220973e0dd39c70
BeneArp <benedetta.arpino@gmail.com>
2024-06-08 11:17:11 +0200

    riferimenti dom

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -1,2 +1,12 @@
 // Gioco dei dadi
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+
+// prendo dal documento i riferimenti
+
+// contenitore dado
+const bottoneTiro = document.getElementById("dado");
+// titolo esito round
+const esitoTrio = document.getElementById("esito");
+// titolo per valore critico
+const tiroCritico = document.getElementById("critico");
